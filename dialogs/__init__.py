d48a2b2cb5e934753e81d5d41f8d51f5af536b37
k1berteftel <kkulis985@gmail.com>
2026-09-08 18:50:17 +0300

    static fix

--- dialogs/__init__.py
+++ dialogs/__init__.py
@@ -1,8 +1,9 @@
 from dialogs.user_dialog.dialog import user_dialog
+from dialogs.gifts_dialog.dialog import gift_dialog
 from dialogs.admin_dialog.dialog import admin_dialog
 from dialogs.sub_dialog.dialog import sub_dialog
 from dialogs.payment_dialog.dialog import payment_dialog
 
 
 def get_dialogs():
-    return [user_dialog, payment_dialog, sub_dialog, admin_dialog]
+    return [user_dialog, gift_dialog, payment_dialog, sub_dialog, admin_dialog]
